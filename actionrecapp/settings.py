04507df6c320f8e198975d19ca19ada62365150c
ganapathi1578 <ganapathi1578@gmail.con>
2025-05-26 15:23:57 +0530

    ready

--- actionrecapp/settings.py
+++ actionrecapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from config import params
+
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
